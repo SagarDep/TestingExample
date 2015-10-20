2f2d6ffdc9bf5c57d16342bb87058f978036d854
sangcomz <sangcomz@naver.com>
2015-10-20 17:28:58 +0900

    init

---
+++ app/src/main/java/com/example/testing/testingexample/Calculator.java
@@ -0,0 +1,20 @@
+package com.example.testing.testingexample;
+
+public class Calculator {
+
+    public double sum(double a, double b){
+        return a+b;
+    }
+
+    public double subtract(double a, double b){
+        return 0;
+    }
+
+    public double divide(double a, double b){
+        return 0;
+    }
+
+    public double multiply(double a, double b){
+        return 0;
+    }
+}
